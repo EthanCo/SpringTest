9abb4b0422990360c4f8d6974d9726c1e14c6d33
Heiko-HP <fenghenyu@163.com>
2020-10-09 07:57:06 +0800

    使用注解

--- my_spring_ioc_anno/src/main/java/com/heiko/dao/impl/UserDaoImpl.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/dao/impl/UserDaoImpl.java
@@ -1,7 +1,10 @@
 package com.heiko.dao.impl;
 
 import com.heiko.dao.UserDao;
+import org.springframework.stereotype.Component;
 
+////<bean id="userDao" class="com.heiko.dao.impl.UserDaoImpl"></bean>
+@Component("userDao")
 public class UserDaoImpl implements UserDao {
     public void save() {
         System.out.println("save running...");
--- my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
@@ -2,8 +2,16 @@ package com.heiko.uservice.impl;
 
 import com.heiko.dao.UserDao;
 import com.heiko.uservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+//<bean id="userService" class="com.heiko.uservice.impl.UserServiceImpl">
+@Component("userService")
 public class UserServiceImpl implements UserService {
+    //<property name="userDao" ref="userDao"></property>
+    @Autowired
+    @Qualifier("userDao")
     private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
