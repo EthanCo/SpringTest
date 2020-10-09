7e5fb26af493c479929b0aca574208b77a2e899f
Heiko-HP <fenghenyu@163.com>
2020-10-09 08:48:33 +0800

    使用注解3

--- my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
@@ -4,13 +4,19 @@ import com.heiko.dao.UserDao;
 import com.heiko.uservice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
 //<bean id="userService" class="com.heiko.uservice.impl.UserServiceImpl">
 //@Component("userService")
 @Service("userService")
+@Scope("prototype")
+//@Scope("singleton")
 public class UserServiceImpl implements UserService {
     //<property name="userDao" ref="userDao"></property>
     @Autowired //按照数据类型 从Spring容器中进行匹配
@@ -24,7 +30,21 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }*/
 
+    @Value("${jdbc.driver}")
+    private String driver;
+
     public void save() {
+        System.out.println(driver);
         userDao.save();
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Service对象的初始化方法");
+    }
+
+    @PreDestroy
+    public void destory(){
+        System.out.println("Service对象的销毁方法");
     }
 }
