20aaa5046d2c72b471add4732278dc2cc238c8fa
Heiko-HP <fenghenyu@163.com>
2020-10-09 09:08:11 +0800

    使用新注解

--- my_spring_ioc_anno/src/main/java/com/heiko/web/UserController.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/web/UserController.java
@@ -1,14 +1,23 @@
 package com.heiko.web;
 
+import com.heiko.config.SpringConfiguration;
 import com.heiko.uservice.impl.UserServiceImpl;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserController {
-    public static void main(String[] args){
-        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+    //通过XML
+    /*public static void main(String[] args){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserServiceImpl userService = app.getBean(UserServiceImpl.class);
         userService.save();
-        app.close();
+    }*/
+
+    //通过@Configuration注解，此时与applicationContext.xml无关
+    public static void main(String[] args){
+        ApplicationContext app = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+        UserServiceImpl userService = app.getBean(UserServiceImpl.class);
+        userService.save();
     }
 }
