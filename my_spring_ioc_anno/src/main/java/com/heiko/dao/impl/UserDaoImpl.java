5271b0ee88994fd4c41b548cbdb4a81eade56f76
Heiko-HP <fenghenyu@163.com>
2020-10-09 08:04:58 +0800

    使用注解2

--- my_spring_ioc_anno/src/main/java/com/heiko/dao/impl/UserDaoImpl.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/dao/impl/UserDaoImpl.java
@@ -1,10 +1,11 @@
 package com.heiko.dao.impl;
 
 import com.heiko.dao.UserDao;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 ////<bean id="userDao" class="com.heiko.dao.impl.UserDaoImpl"></bean>
-@Component("userDao")
+//@Component("userDao")
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
     public void save() {
         System.out.println("save running...");
--- my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
+++ my_spring_ioc_anno/src/main/java/com/heiko/uservice/impl/UserServiceImpl.java
@@ -4,19 +4,25 @@ import com.heiko.dao.UserDao;
 import com.heiko.uservice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 //<bean id="userService" class="com.heiko.uservice.impl.UserServiceImpl">
-@Component("userService")
+//@Component("userService")
+@Service("userService")
 public class UserServiceImpl implements UserService {
     //<property name="userDao" ref="userDao"></property>
-    @Autowired
-    @Qualifier("userDao")
+    @Autowired //按照数据类型 从Spring容器中进行匹配
+    //@Qualifier("userDao") 是按照id值从容器中进行匹配的，但是此处要结合@Autowired一起用
+    @Resource(name = "userDao") //相当于@Qualifier+@Autowired
     private UserDao userDao;
 
+    /*
+    使用注解 可注释掉
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
-    }
+    }*/
 
     public void save() {
         userDao.save();
