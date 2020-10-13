24c6f3eed722fec91fe5c7b3f96ea0f266977fed
Heiko-HP <fenghenyu@163.com>
2020-10-13 08:40:45 +0800

    aop 通知的类型

--- my_spring_aop/src/main/java/com/heiko/aop/MyAspect.java
+++ my_spring_aop/src/main/java/com/heiko/aop/MyAspect.java
@@ -1,7 +1,28 @@
 package com.heiko.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MyAspect {
     public void before() {
         System.out.println("前置增强...");
+    }
+
+    public void afterReturning() {
+        System.out.println("后置增强...");
+    }
+
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("环绕前增强...");
+        pjp.proceed(); //切点方法
+        System.out.println("环绕后增强...");
+        return pjp;
+    }
+
+    public void afterThrowing(){
+        System.out.println("异常抛出增强...");
+    }
+
+    public void after(){
+        System.out.println("最终增强...");
     }
 }
--- my_spring_aop/src/main/java/com/heiko/aop/Target.java
+++ my_spring_aop/src/main/java/com/heiko/aop/Target.java
@@ -2,6 +2,7 @@ package com.heiko.aop;
 
 public class Target implements TargetInterface {
     public void save() {
+        //int i = 1/0;
         System.out.println("save running...");
     }
 }
