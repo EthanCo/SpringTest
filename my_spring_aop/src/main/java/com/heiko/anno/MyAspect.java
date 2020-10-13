e6c2554b0e549b69901b3b7352d0ae0a252f6ee4
Heiko-HP <fenghenyu@163.com>
2020-10-14 07:40:28 +0800

    aop 注解

--- my_spring_aop/src/main/java/com/heiko/anno/MyAspect.java
+++ my_spring_aop/src/main/java/com/heiko/anno/MyAspect.java
@@ -13,7 +13,12 @@ public class MyAspect {
         System.out.println("前置增强...");
     }
 
-    @AfterReturning("execution(public void com.heiko.anno.Target.save())")
+    /*@AfterReturning("execution(public void com.heiko.anno.Target.save())")
+    public void afterReturning() {
+        System.out.println("后置增强...");
+    }*/
+
+    @AfterReturning("pointcut()")
     public void afterReturning() {
         System.out.println("后置增强...");
     }
@@ -31,8 +36,18 @@ public class MyAspect {
         System.out.println("异常抛出增强...");
     }
 
-    @After("execution(public void com.heiko.anno.Target.save())")
+    /*@After("execution(public void com.heiko.anno.Target.save())")
     public void after(){
         System.out.println("最终增强...");
+    }*/
+
+    @After("MyAspect.pointcut()")
+    public void after(){
+        System.out.println("最终增强...");
+    }
+
+    @Pointcut("execution(public void com.heiko.anno.Target.save())")
+    public void pointcut(){
+
     }
 }
