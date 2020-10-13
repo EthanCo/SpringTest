3c1d146efb45f6d9641bb42e4c650b4abf886f22
Heiko-HP <fenghenyu@163.com>
2020-10-14 07:37:57 +0800

    aop 注解

--- my_spring_aop/src/main/java/com/heiko/anno/MyAspect.java
+++ my_spring_aop/src/main/java/com/heiko/anno/MyAspect.java
@@ -1,8 +1,7 @@
 package com.heiko.anno;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component("myAspect")
@@ -14,10 +13,12 @@ public class MyAspect {
         System.out.println("前置增强...");
     }
 
+    @AfterReturning("execution(public void com.heiko.anno.Target.save())")
     public void afterReturning() {
         System.out.println("后置增强...");
     }
 
+    @Around("execution(public void com.heiko.anno.Target.save())")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("环绕前增强...");
         pjp.proceed(); //切点方法
@@ -25,10 +26,12 @@ public class MyAspect {
         return pjp;
     }
 
+    @AfterReturning("execution(public void com.heiko.anno.Target.save())")
     public void afterThrowing(){
         System.out.println("异常抛出增强...");
     }
 
+    @After("execution(public void com.heiko.anno.Target.save())")
     public void after(){
         System.out.println("最终增强...");
     }
