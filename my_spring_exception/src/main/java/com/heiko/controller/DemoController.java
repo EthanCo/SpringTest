f94068c8426a74db7380f67843b4980fb09c7f01
Heiko <fenghenyu@163.com>
2020-11-06 11:26:50 +0800

    SpringMVC 异常处理机制

--- my_spring_exception/src/main/java/com/heiko/controller/DemoController.java
+++ my_spring_exception/src/main/java/com/heiko/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.heiko.controller;
 
+import com.heiko.exception.MyException;
 import com.heiko.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,13 @@ public class DemoController {
     private DemoService demoService;
 
     @RequestMapping("/test")
-    public String show() {
+    public String show() throws MyException {
         System.out.println("show running...");
-        demoService.show1();
+        //demoService.show1();
+        //demoService.show2();
+        //demoService.show3();
+        //demoService.show4();
+        demoService.show5();
         return "index";
     }
 }
